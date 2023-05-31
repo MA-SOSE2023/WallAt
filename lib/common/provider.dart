c742740ca9f1fd7beb22ab22444ee6abdffa266f
biselli-mar <ma741bis@htwg-konstanz.de>
2023-05-31 18:20:42 +0200

    #5-adds descriptive doc comments for providers

--- lib/common/provider.dart
+++ lib/common/provider.dart
@@ -4,7 +4,10 @@ import 'custom_widgets/bottom_nav_bar/bottom_nav_bar_view.dart';
 import 'custom_widgets/bottom_nav_bar/bottom_nav_bar_controller.dart';
 import 'custom_widgets/bottom_nav_bar/bottom_nav_bar_model.dart';
 
+/// Flutter Riverpod providers
 class Providers {
+  /// Provider for [CustomBottomNavBar]
+  /// - Provides a [CustomBottomNavBarController] for a [CustomBottomNavBarModel]
   static final StateNotifierProvider<CustomBottomNavBarController,
           CustomBottomNavBarModel> customBottomNavBarControllerProvider =
       StateNotifierProvider<CustomBottomNavBarController,
