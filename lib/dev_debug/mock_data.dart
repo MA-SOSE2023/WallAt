e6f2577c2689766d26d83a3a56a7739f30324afe
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-02 13:40:33 +0200

    #8-partially reverts previous change: - adss mock implementation for SingleItemController - move mock data directly inside new mock controller

--- lib/dev_debug/mock_data.dart
+++
@@ -1,7 +0,0 @@
-import '../pages/single_item/model/single_item.dart';
-
-const exampleSingleItem = SingleItem(
-  title: 'Example Title',
-  description: 'Example Description',
-  image: 'assets/images/example_document.png',
-);
