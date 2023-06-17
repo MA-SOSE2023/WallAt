af25127c9e5e63773cec4a7db67a00a18cb30735
Smokey95 <97231524+Smokey95@users.noreply.github.com>
2023-06-17 13:59:17 +0200

    Add develop main for camera implementation

---
+++ lib/dev_debug_camera.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'pages/camera/camera_view.dart';
+
+void main() => runApp(const ProviderScope(child: MyApp()));
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+        home: Scaffold(
+      body: TakePictureScreen(),
+      bottomNavigationBar: CupertinoTabBar(
+        iconSize: 30,
+        onTap: (index) => {},
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: "Favorites"),
+        ],
+      ),
+    ));
+  }
+}
