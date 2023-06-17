29e6efbb11e19688f1e287070bb5cb6d7c113a9c
Smokey95 <97231524+Smokey95@users.noreply.github.com>
2023-06-17 15:01:49 +0200

    #9 Update develop main

--- lib/dev_debug_camera.dart
+++ lib/dev_debug_camera.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
         home: Scaffold(
-      body: TakePictureScreen(),
+      body: const TakePictureScreen(),
       bottomNavigationBar: CupertinoTabBar(
         iconSize: 30,
         onTap: (index) => {},
