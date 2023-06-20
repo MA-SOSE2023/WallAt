79ac594872f76c114082ab9cdf275717e3d7a6e8
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-20 21:16:38 +0200

    #22-adds HomeScreen with floating camera button

---
+++ lib/pages/home/home_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../router/router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: const CupertinoNavigationBar(
+          middle: Text('Home'),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 15.0, right: 10.0),
+          child: FloatingActionButton(
+            foregroundColor: CupertinoTheme.of(context).primaryContrastingColor,
+            backgroundColor: CupertinoTheme.of(context).primaryColor,
+            onPressed: () =>
+                Routers.globalRouterDelegate.beamToNamed('/camera'),
+            child: const Icon(
+              CupertinoIcons.camera,
+              size: 30,
+            ),
+          ),
+        ),
+        body: const Placeholder());
+  }
+}
