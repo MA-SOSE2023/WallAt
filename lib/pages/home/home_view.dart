45da9128ce3555e8001db8c422a27ac1603b32f2
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-21 23:00:32 +0200

    #6-switch from ListSection to Container & Column: - reason being that we can use Expanded instead of a SizedBox - this reduces risk of Overflow

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -10,7 +10,6 @@ import '/common/custom_widgets/all_custom_widgets.dart'
     show EventCard, cameraButtonHeroTag;
 import '/router/router.dart';
 import '/pages/single_item/model/item_event.dart';
-import '/pages/single_item/model/single_item.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
@@ -49,61 +48,88 @@ class HomeScreen extends ConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            FlutterCarousel(
-              items: eventCards,
-              options: CarouselOptions(
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                enlargeCenterPage: true,
-                showIndicator: true,
-                slideIndicator: CircularWaveSlideIndicator(
-                  currentIndicatorColor: CupertinoDynamicColor.resolve(
-                      CupertinoColors.activeBlue, context),
-                  indicatorBackgroundColor: CupertinoDynamicColor.resolve(
-                      CupertinoColors.systemGrey3, context),
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height / 5),
+              child: FlutterCarousel(
+                items: eventCards,
+                options: CarouselOptions(
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  enlargeCenterPage: true,
+                  showIndicator: true,
+                  slideIndicator: CircularWaveSlideIndicator(
+                    currentIndicatorColor: CupertinoDynamicColor.resolve(
+                        CupertinoColors.activeBlue, context),
+                    indicatorBackgroundColor: CupertinoDynamicColor.resolve(
+                        CupertinoColors.systemGrey3, context),
+                  ),
+                  viewportFraction: 0.85,
+                  height: double.infinity,
                 ),
-                viewportFraction: 0.85,
-                height: MediaQuery.of(context).size.height / 5,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
-              child: CupertinoListSection.insetGrouped(
-                header: Text(
-                  'Frequently Used',
-                  style: CupertinoTheme.of(context).textTheme.textStyle,
-                ),
-                decoration: BoxDecoration(
-                  color: CupertinoTheme.of(context).scaffoldBackgroundColor,
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                children: [
-                  ConstrainedBox(
-                    constraints: BoxConstraints(
-                        maxHeight: MediaQuery.of(context).size.height / 2),
-                    child: ListView(
-                      children: [
-                        ...documentCards
-                            .sublist(0, documentCards.length - 1)
-                            .map(
-                          (card) {
-                            return Column(
-                              children: [
-                                card,
-                                const Divider(
-                                  height: 0,
-                                  thickness: 1,
-                                  indent: 64,
-                                ),
-                              ],
-                            );
-                          },
-                        ).toList(),
-                        documentCards.last,
-                      ],
-                    ),
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height -
+                    (MediaQuery.of(context).size.height / 5) -
+                    140,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        const BorderRadius.vertical(top: Radius.circular(25)),
+                    color: CupertinoDynamicColor.resolve(
+                        CupertinoColors.systemGroupedBackground, context),
                   ),
-                ],
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding:
+                            EdgeInsets.only(left: 20.0, top: 20.0, bottom: 5.0),
+                        child: Text(
+                          'Frequently Used',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                              left: 10, right: 10, bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: CupertinoTheme.of(context)
+                                .scaffoldBackgroundColor,
+                          ),
+                          child: ListView(
+                            children: [
+                              ...documentCards
+                                  .sublist(0, documentCards.length - 1)
+                                  .map(
+                                (card) {
+                                  return Column(
+                                    children: [
+                                      card,
+                                      const Divider(
+                                        height: 0,
+                                        thickness: 1,
+                                        indent: 64,
+                                      ),
+                                    ],
+                                  );
+                                },
+                              ).toList(),
+                              documentCards.last,
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
