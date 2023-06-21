fb288d74bec55e6d73613dbf0a6e2ec521f62a5c
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-21 19:46:05 +0200

    #6.#26-adds EventCard carousel to home screen

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 
 import 'home_model.dart';
 import '/common/provider.dart';
-import '/common/custom_widgets/camera_button_hero_destination.dart';
+import '/common/custom_widgets/all_custom_widgets.dart'
+    show EventCard, cameraButtonHeroTag;
 import '/router/router.dart';
 import '/pages/single_item/model/item_event.dart';
 
@@ -14,26 +16,54 @@ class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     HomeModel model = ref.watch(Providers.homeControllerProvider);
+    final List<EventCard> eventCards = model.events
+        .map((event) => EventCard(
+              event: event,
+            ))
+        .toList();
     return Scaffold(
-        appBar: const CupertinoNavigationBar(
-          middle: Text('Home'),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(bottom: 15.0, right: 10.0),
-          child: FloatingActionButton(
-            foregroundColor: CupertinoTheme.of(context).primaryContrastingColor,
-            backgroundColor: CupertinoTheme.of(context).primaryColor,
-            onPressed: () =>
-                Routers.globalRouterDelegate.beamToNamed('/camera'),
-            heroTag: cameraButtonHeroTag,
-            child: const Icon(
-              CupertinoIcons.camera,
-              size: 30,
-            ),
+      appBar: const CupertinoNavigationBar(
+        middle: Text('Home'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 15.0, right: 10.0),
+        child: FloatingActionButton(
+          foregroundColor: CupertinoTheme.of(context).primaryContrastingColor,
+          backgroundColor: CupertinoTheme.of(context).primaryColor,
+          onPressed: () => Routers.globalRouterDelegate.beamToNamed('/camera'),
+          heroTag: cameraButtonHeroTag,
+          child: const Icon(
+            CupertinoIcons.camera,
+            size: 30,
           ),
         ),
-        body: const Placeholder());
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 4,
+            child: FlutterCarousel(
+              items: eventCards,
+              options: CarouselOptions(
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  enlargeCenterPage: true,
+                  showIndicator: true,
+                  slideIndicator: CircularWaveSlideIndicator(
+                    currentIndicatorColor: CupertinoDynamicColor.resolve(
+                        CupertinoColors.activeBlue, context),
+                    indicatorBackgroundColor: CupertinoDynamicColor.resolve(
+                        CupertinoColors.systemGrey3, context),
+                  ),
+                  viewportFraction: 0.85),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
