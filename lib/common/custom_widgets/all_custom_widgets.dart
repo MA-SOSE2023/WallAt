acf902742e20b1ee477b3545c45aee6486c004f1
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-21 18:08:31 +0200

    export recent additions to custom widgets

--- lib/common/custom_widgets/all_custom_widgets.dart
+++ lib/common/custom_widgets/all_custom_widgets.dart
@@ -2,3 +2,6 @@
 
 export '/common/custom_widgets/bottom_nav_bar/bottom_nav_bar_view.dart';
 export '/common/custom_widgets/event_container.dart';
+export '/common/custom_widgets/camera_button_hero_destination.dart';
+export '/common/custom_widgets/event_card_view.dart';
+export '/common/custom_widgets/calendar_button.dart';
