c6be9ab10772f839dac1a76f51f52c8cc4f582c2
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-22 00:11:04 +0200

    #30-use doc card list in favorites view with mock

--- lib/pages/favorites/favorites_view.dart
+++ lib/pages/favorites/favorites_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+import '/common/custom_widgets/all_custom_widgets.dart'
+    show DocumentCardContainerList;
 import '/common/custom_widgets/camera_button_hero_destination.dart';
 
 class FavoritesScren extends StatelessWidget {
@@ -7,16 +10,56 @@ class FavoritesScren extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> itemIds = <String>[
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8'
+    ];
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(
         middle: Text('Favorites'),
       ),
       child: Stack(
-        children: const [
+        children: [
+          const CameraButtonHeroDestination(),
           SafeArea(
-            child: Placeholder(),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height,
+              ),
+              child: Column(
+                children: [
+                  DocumentCardContainerList(itemIds: itemIds),
+                  // Search bar for filtering items
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+                    child: SizedBox(
+                      height: 50,
+                      child: CupertinoTextField(
+                        placeholder: 'Search',
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: CupertinoTheme.of(context)
+                              .scaffoldBackgroundColor,
+                          borderRadius: BorderRadius.circular(25),
+                          border: Border.all(
+                            color: CupertinoDynamicColor.resolve(
+                                CupertinoColors.label, context),
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          CameraButtonHeroDestination(),
         ],
       ),
     );
