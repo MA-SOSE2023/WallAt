e1bfafa111ef412478dd1af3b44bc2bb4b1290f9
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-22 22:47:12 +0200

    #30-adds custom display that no elements exist yet

---
+++ lib/common/custom_widgets/no_elements_message.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class NoElementsMessage extends StatelessWidget {
+  const NoElementsMessage({String message = 'No elements yet.', super.key})
+      : _message = message;
+
+  final String _message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Center(
+        child: Text(
+          _message,
+          style: TextStyle(
+            color: CupertinoDynamicColor.resolve(
+              CupertinoColors.inactiveGray,
+              context,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
