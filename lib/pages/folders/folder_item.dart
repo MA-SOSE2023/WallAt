f50dc6c436b31d4c3301afa520347f768b24a98d
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-24 15:31:33 +0200

    #39-removes unused named constructors

--- lib/pages/folders/folder_item.dart
+++ lib/pages/folders/folder_item.dart
@@ -1,5 +1,4 @@
 import 'folder_model.dart';
-import '/pages/single_item/model/item_event.dart';
 import '/pages/single_item/model/single_item.dart';
 
 abstract class FolderItem {
@@ -16,54 +15,6 @@ abstract class FolderItem {
       assert(folder != null);
     }
   }
-
-  FolderItem.folder({
-    required Folder folder,
-  }) : this(
-          isLeaf: false,
-          folder: folder,
-        );
-
-  FolderItem.item(
-    SingleItem item,
-  ) : this(
-          isLeaf: true,
-          item: item,
-        );
-
-  FolderItem.folderFromValues({
-    required String id,
-    required String title,
-    required List<FolderItem> contents,
-  }) : this(
-          isLeaf: false,
-          folder: Folder(
-            id: id,
-            title: title,
-            contents: contents,
-          ),
-        );
-
-  FolderItem.itemFromValues({
-    required String id,
-    required String title,
-    required String description,
-    required String image,
-    required bool isFavorite,
-    required List<ItemEvent> events,
-    DateTime? currentSelectedDate,
-  }) : this(
-          isLeaf: true,
-          item: SingleItem(
-            id: id,
-            title: title,
-            description: description,
-            image: image,
-            isFavorite: isFavorite,
-            events: events,
-            currentSelectedDate: currentSelectedDate,
-          ),
-        );
 
   final bool _isLeaf;
   final SingleItem? _item;
