fb8a0b0c0860af601b4cdb27631e02c9798b6edc
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-27 00:57:49 +0200

    #42.#44-implement isar folder DIO

--- lib/common/services/persistence/isar/dio/isar_folder_dio.dart
+++ lib/common/services/persistence/isar/dio/isar_folder_dio.dart
@@ -5,12 +5,10 @@ import '/common/services/persistence/isar/schemas/isar_folder.dart';
 import '/common/services/persistence/persistence_service.dart';
 
 class IsarFolderDio extends FolderDio {
-  IsarFolderDio({required Future<Isar> db, required int rootFolderId})
-      : _db = db,
-        _rootFolderId = rootFolderId;
+  IsarFolderDio({required Future<Isar> db}) : _db = db;
 
   final Future<Isar> _db;
-  final int _rootFolderId;
+  int? _rootFolderId;
 
   @override
   Future<Folder> create({required String title, int? parentFolderId}) async {
@@ -18,7 +16,8 @@ class IsarFolderDio extends FolderDio {
     return isar.writeTxnSync(() async {
       Id createdFolder = isar.isarFolders.putSync(IsarFolder()
         ..title = title
-        ..parentFolder.value = await _read(parentFolderId ?? _rootFolderId));
+        ..parentFolder.value = await _isarRead(
+            parentFolderId ?? _rootFolderId ?? (await createOrFindRoot())));
       return Folder(
         id: createdFolder,
         title: title,
@@ -28,31 +27,61 @@ class IsarFolderDio extends FolderDio {
   }
 
   @override
-  Future<void> delete(Id id) {
-    // TODO: implement delete
-    throw UnimplementedError();
+  Future<Id> createOrFindRoot() async {
+    final isar = await _db;
+    IsarFolder? root =
+        await isar.isarFolders.filter().isRootEqualTo(true).findFirst();
+    root ??= (await isar.isarFolders.get(await _createRoot()))!;
+
+    _rootFolderId = root.id;
+    return root.id;
+  }
+
+  Future<Id> _createRoot() async {
+    final isar = await _db;
+    return isar.writeTxnSync(() async => isar.isarFolders.putSync(
+          IsarFolder()
+            ..title = 'Folders'
+            ..isRoot = true
+            ..parentFolder.value = null,
+        ));
   }
 
   @override
-  Future<Folder> read(Id id) {
-    // TODO: implement read
-    throw UnimplementedError();
+  Future<bool> delete(Id id) async {
+    final isar = await _db;
+    return isar.isarFolders.delete(id);
   }
 
-  Future<IsarFolder?> _read(Id id) async {
+  @override
+  Future<Folder?> read(Id id) async {
+    final isarFolder = _isarRead(id);
+    return isarFolder.then((isarFolder) => isarFolder?.toFolder());
+  }
+
+  Future<IsarFolder?> _isarRead(Id id) async {
     final isar = await _db;
     return isar.isarFolders.get(id);
   }
 
   @override
-  Future<List<Folder>> readAll() {
-    // TODO: implement readAll
-    throw UnimplementedError();
+  Future<List<Folder>> readAll() async {
+    final isar = await _db;
+    return isar.isarFolders.where().findAll().then((folders) =>
+        folders.map((isarFolder) => isarFolder.toFolder()).toList());
   }
 
   @override
-  Future<void> update(Folder item) {
-    // TODO: implement update
-    throw UnimplementedError();
+  Future<void> update(Folder item) async {
+    final isar = await _db;
+    IsarFolder isarFolder = (await _isarRead(item.id))!;
+    isar.isarFolders.put(isarFolder..title = item.title);
+  }
+
+  @override
+  Future<void> move(Folder folder, Folder newParent) async {
+    final isar = await _db;
+    isar.isarFolders.put((await _isarRead(folder.id))!
+      ..parentFolder.value = await _isarRead(newParent.id));
   }
 }
