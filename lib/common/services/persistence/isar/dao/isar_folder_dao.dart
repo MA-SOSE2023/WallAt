fadd23d5ff522a8ee10ea5e0db1421693035abc0
biselli-mar <ma741bis@htwg-konstanz.de>
2023-06-28 23:55:04 +0200

    #42-use future chaining for db in DAOs

--- lib/common/services/persistence/isar/dao/isar_folder_dao.dart
+++ lib/common/services/persistence/isar/dao/isar_folder_dao.dart
@@ -5,85 +5,78 @@ import '/common/services/persistence/isar/schemas/isar_folder.dart';
 import '/common/services/persistence/persistence_service.dart';
 
 class IsarFolderDao extends FolderDao {
-  IsarFolderDao({required Future<Isar> db}) : _db = db;
+  IsarFolderDao({
+    required Future<Isar> db,
+  }) : _db = db;
 
   final Future<Isar> _db;
   int? _rootFolderId;
 
   @override
-  Future<Folder> create({required String title, int? parentFolderId}) async {
-    final isar = await _db;
-    return isar.writeTxnSync(() async {
-      Id createdFolder = isar.isarFolders.putSync(IsarFolder()
-        ..title = title
-        ..parentFolder.value = await _isarRead(
-            parentFolderId ?? _rootFolderId ?? (await createOrFindRoot())));
-      return Folder(
-        id: createdFolder,
-        title: title,
-        contents: [],
+  Future<Folder> create({required String title, int? parentFolderId}) =>
+      _db.then(
+        (isar) async => _isarRead(
+                parentFolderId ?? _rootFolderId ?? (await createOrFindRoot()))
+            .then(
+          (parentFolder) => isar.writeTxnSync(() async {
+            Id createdFolder = isar.isarFolders.putSync(IsarFolder()
+              ..title = title
+              ..parentFolder.value = parentFolder);
+            return Folder(
+              id: createdFolder,
+              title: title,
+              contents: [],
+            );
+          }),
+        ),
       );
-    });
-  }
 
   @override
-  Future<Id> createOrFindRoot() async {
-    final isar = await _db;
-    IsarFolder? root =
-        await isar.isarFolders.filter().isRootEqualTo(true).findFirst();
-    root ??= (await isar.isarFolders.get(await _createRoot()))!;
+  Future<Id> createOrFindRoot() => _db.then((isar) async {
+        IsarFolder? root =
+            isar.isarFolders.filter().isRootEqualTo(true).findFirstSync();
+        root ??= isar.isarFolders.getSync(await _createRoot())!;
 
-    _rootFolderId = root.id;
-    return root.id;
-  }
+        _rootFolderId = root.id;
+        return root.id;
+      });
 
-  Future<Id> _createRoot() async {
-    final isar = await _db;
-    return isar.writeTxnSync(() async => isar.isarFolders.putSync(
-          IsarFolder()
-            ..title = 'Folders'
-            ..isRoot = true
-            ..parentFolder.value = null,
-        ));
-  }
+  Future<Id> _createRoot() =>
+      _db.then((isar) => isar.writeTxnSync(() async => isar.isarFolders.putSync(
+            IsarFolder()
+              ..title = 'Folders'
+              ..isRoot = true
+              ..parentFolder.value = null,
+          )));
 
   @override
-  Future<bool> delete(Id id) async {
-    final isar = await _db;
-    return isar.isarFolders.delete(id);
-  }
+  Future<bool> delete(Id id) => _db.then((isar) => isar.isarFolders.delete(id));
 
   @override
-  Future<Folder?> read(Id id) async {
-    final isarFolder = _isarRead(id);
-    return isarFolder.then((isarFolder) => isarFolder?.toFolder());
-  }
+  Future<Folder?> read(Id id) =>
+      _isarRead(id).then((isarFolder) => isarFolder?.toFolder());
 
-  Future<IsarFolder?> _isarRead(Id id) async {
-    final isar = await _db;
-    return isar.isarFolders.get(id);
-  }
+  Future<IsarFolder?> _isarRead(Id id) =>
+      _db.then((isar) => isar.isarFolders.get(id));
 
   @override
-  Future<List<Folder>> readAll() async {
-    final isar = await _db;
-    return isar.isarFolders.where().findAll().then((folders) =>
-        folders.map((isarFolder) => isarFolder.toFolder()).toList());
-  }
+  Future<List<Folder>> readAll() =>
+      _db.then((isar) => isar.isarFolders.where().findAll().then((folders) =>
+          folders.map((isarFolder) => isarFolder.toFolder()).toList()));
 
   @override
-  Future<void> update(Folder item) async {
-    final isar = await _db;
-    IsarFolder? isarFolder = (await _isarRead(item.id));
-    if (isarFolder != null) {
-      isar.isarFolders.put(isarFolder..title = item.title);
-    }
-  }
+  Future<void> update(Folder item) => _isarRead(item.id).then((isarFolder) {
+        if (isarFolder != null) {
+          return _db.then((isar) => isar.writeTxnSync(() async {
+                isar.isarFolders.putSync(isarFolder..title = item.title);
+              }));
+        }
+      });
 
   @override
-  Future<void> move(Folder folder, Folder newParent) async {
-    final isar = await _db;
-    isar.isarFolders.put((await _isarRead(folder.id))!
-      ..parentFolder.value = await _isarRead(newParent.id));
-  }
+  Future<void> move(Folder folder, Folder newParent) =>
+      _db.then((isar) => isar.writeTxnSync(() async {
+            isar.isarFolders.putSync((await _isarRead(folder.id))!
+              ..parentFolder.value = await _isarRead(newParent.id));
+          }));
 }
--- lib/common/services/persistence/isar/dao/isar_item_event_dao.dart
+++ lib/common/services/persistence/isar/dao/isar_item_event_dao.dart
@@ -12,75 +12,67 @@ class IsarItemEventDao extends ItemEventDao {
   final Future<Isar> _db;
 
   @override
-  Future<ItemEvent> create(
-      {required Event event, required int parentItemId}) async {
-    final isar = await _db;
-    return isar.writeTxnSync(() async {
-      Id createdEvent = isar.isarItemEvents.putSync(IsarItemEvent()
-        ..title = event.title ?? ''
-        ..description = event.description ?? ''
-        ..start = event.start!
-        ..end = event.end!
-        ..parentItem.value = (await _isarReadParent(parentItemId))!);
-      return ItemEvent(
-        id: createdEvent,
-        event: event,
-        parentId: parentItemId,
+  Future<ItemEvent> create({required Event event, required int parentItemId}) =>
+      _db.then(
+        (isar) => isar.writeTxnSync(
+          () => _isarReadParent(parentItemId).then(
+            (parentItem) {
+              parentItem!;
+              Id createdEvent = isar.isarItemEvents.putSync(IsarItemEvent()
+                ..title = event.title ?? ''
+                ..description = event.description ?? ''
+                ..start = event.start!
+                ..end = event.end!
+                ..parentItem.value = parentItem);
+              return ItemEvent(
+                id: createdEvent,
+                event: event,
+                parentId: parentItemId,
+              );
+            },
+          ),
+        ),
       );
-    });
-  }
 
-  Future<IsarItemEvent?> _isarRead(int id) async {
-    final isar = await _db;
-    return isar.isarItemEvents.get(id);
-  }
+  Future<IsarItemEvent?> _isarRead(int id) =>
+      _db.then((isar) => isar.isarItemEvents.get(id));
 
-  Future<IsarSingleItem?> _isarReadParent(int id) async {
-    final isar = await _db;
-    return isar.isarSingleItems.get(id);
-  }
+  Future<IsarSingleItem?> _isarReadParent(int id) =>
+      _db.then((isar) => isar.isarSingleItems.get(id));
 
   @override
-  Future<bool> delete(Id id) async {
-    final isar = await _db;
-    return isar.isarItemEvents.delete(id);
-  }
+  Future<bool> delete(Id id) =>
+      _db.then((isar) => isar.isarItemEvents.delete(id));
 
   @override
-  Future<ItemEvent?> read(Id id) async {
-    final isarEvent = _isarRead(id);
-    return isarEvent.then((isarEvent) => isarEvent?.toItemEvent());
-  }
+  Future<ItemEvent?> read(Id id) =>
+      _isarRead(id).then((isarEvent) => isarEvent?.toItemEvent());
 
   @override
-  Future<List<ItemEvent>> readAll() async {
-    final isar = await _db;
-    return isar.isarItemEvents.where().findAll().then((isarEvents) =>
-        isarEvents.map((isarEvent) => isarEvent.toItemEvent()).toList());
-  }
+  Future<List<ItemEvent>> readAll() => _db.then((isar) => isar.isarItemEvents
+      .where()
+      .findAll()
+      .then((isarEvents) =>
+          isarEvents.map((isarEvent) => isarEvent.toItemEvent()).toList()));
 
   @override
-  Future<List<ItemEvent>> readAllSoon(Duration soonDuration) async {
-    final isar = await _db;
-    return isar.isarItemEvents
-        .filter()
-        .startGreaterThan(DateTime.now())
-        .endLessThan(DateTime.now().add(soonDuration))
-        .findAll()
-        .then((isarEvents) =>
-            isarEvents.map((isarEvent) => isarEvent.toItemEvent()).toList());
-  }
+  Future<List<ItemEvent>> readAllSoon(Duration soonDuration) =>
+      _db.then((isar) => isar.isarItemEvents
+          .filter()
+          .startGreaterThan(DateTime.now())
+          .endLessThan(DateTime.now().add(soonDuration))
+          .findAll()
+          .then((isarEvents) =>
+              isarEvents.map((isarEvent) => isarEvent.toItemEvent()).toList()));
 
   @override
-  Future<void> update(ItemEvent item) async {
-    final isar = await _db;
-    IsarItemEvent? isarEvent = await _isarRead(item.id);
-    if (isarEvent != null) {
-      isar.isarItemEvents.put(isarEvent
-        ..title = item.event.title ?? ''
-        ..description = item.event.description ?? ''
-        ..start = item.event.start!
-        ..end = item.event.end!);
-    }
-  }
+  Future<void> update(ItemEvent item) => _isarRead(item.id).then((isarEvent) {
+        if (isarEvent != null) {
+          _db.then((isar) => isar.isarItemEvents.put(isarEvent
+            ..title = item.event.title ?? ''
+            ..description = item.event.description ?? ''
+            ..start = item.event.start!
+            ..end = item.event.end!));
+        }
+      });
 }
