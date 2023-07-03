d343679e91501480dd399dfedc88d4643e4288a4
biselli-mar <ma741bis@htwg-konstanz.de>
2023-07-04 00:46:18 +0200

    display right title in date pickers for event

--- lib/common/custom_widgets/calendar_button/calendar_button.dart
+++ lib/common/custom_widgets/calendar_button/calendar_button.dart
@@ -12,7 +12,7 @@ import '/common/custom_widgets/all_custom_widgets.dart'
     show DatePicker, SelectCalendarPopup;
 import '/common/provider.dart';
 
-class CalendarButton extends StatelessWidget {
+class CalendarButton extends ConsumerWidget {
   const CalendarButton({required Function(Event event) onSave, Key? key})
       : _onSave = onSave,
         super(key: key);
@@ -20,144 +20,139 @@ class CalendarButton extends StatelessWidget {
   final Function(Event event) _onSave;
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (BuildContext context, WidgetRef ref, Widget? child) {
-        SettingsController controller =
-            ref.read(Providers.settingsControllerProvider.notifier);
-        void showAddEventDialog(
-          BuildContext context,
-        ) {
-          final settings = ref.watch(Providers.settingsControllerProvider);
+  Widget build(BuildContext context, WidgetRef ref) {
+    SettingsController controller =
+        ref.read(Providers.settingsControllerProvider.notifier);
+    void showAddEventDialog(
+      BuildContext context,
+    ) {
+      final settings = ref.watch(Providers.settingsControllerProvider);
 
-          if (settings.calendar == null) {
-            showCupertinoDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return SelectCalendarPopup(onCalendarSelected: (calendar) {
-                    controller.setUsedCalendar(calendar);
-                  });
-                });
-          } else {
-            showCupertinoDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return Consumer(builder:
-                    (BuildContext context, WidgetRef ref, Widget? child) {
-                  CalendarModel calendarModel =
-                      ref.watch(Providers.calendarButtonControllerProvider);
-                  CalendarButtonController calendarButtonController = ref.read(
-                      Providers.calendarButtonControllerProvider.notifier);
-                  return CupertinoAlertDialog(
-                    title: const Padding(
-                      padding: EdgeInsets.only(bottom: 16),
-                      child: Text('Add Event'),
-                    ),
-                    content: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 16),
-                          child: CupertinoTextField(
-                            placeholder: 'Event Title',
-                            onChanged: (value) {
-                              calendarButtonController.setTitle(value);
-                            },
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 16),
-                          child: CupertinoTextField(
-                            placeholder: 'Event Description',
-                            onChanged: (value) {
-                              calendarButtonController.setDescription(value);
-                            },
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 16),
-                          child: CupertinoTextField(
-                            placeholder: 'Enter reminder minutes',
-                            keyboardType: TextInputType.number,
-                            onChanged: (value) {
-                              calendarButtonController
-                                  .setReminderMinutes(int.parse(value));
-                            },
-                            inputFormatters: <TextInputFormatter>[
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                          ),
-                        ),
-                        const Text('Recurrence'),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 16),
-                          child: CupertinoSlidingSegmentedControl<int>(
-                            groupValue: calendarModel.selectedRecurrenceIndex,
-                            children: {
-                              for (int i = 0; i < recurrenceOptions.length; i++)
-                                i: Text(
-                                    style: const TextStyle(
-                                      fontSize: 10,
-                                    ),
-                                    recurrenceOptions.values.elementAt(i)),
-                            },
-                            onValueChanged: (int? value) {
-                              calendarButtonController
-                                  .setRecurrenceIndex(value ?? 0);
-                            },
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 16),
-                          child: DatePicker(
-                            description: "Start Time",
-                            dateTime: calendarModel.startDate!,
-                            onDateTimeChanged:
-                                calendarButtonController.setStartDate,
-                          ),
-                        ),
-                        DatePicker(
-                          description: "End Time",
-                          dateTime: calendarModel.endDate!,
-                          onDateTimeChanged:
-                              calendarButtonController.setEndDate,
-                        ),
-                      ],
-                    ),
-                    actions: <Widget>[
-                      CupertinoDialogAction(
-                        child: const Text('Cancel'),
-                        onPressed: () {
-                          Navigator.pop(context);
+      if (settings.calendar == null) {
+        showCupertinoDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return SelectCalendarPopup(onCalendarSelected: (calendar) {
+                controller.setUsedCalendar(calendar);
+              });
+            });
+      } else {
+        showCupertinoDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Consumer(
+                builder: (BuildContext context, WidgetRef ref, Widget? child) {
+              CalendarModel calendarModel =
+                  ref.watch(Providers.calendarButtonControllerProvider);
+              CalendarButtonController calendarButtonController =
+                  ref.read(Providers.calendarButtonControllerProvider.notifier);
+              return CupertinoAlertDialog(
+                title: const Padding(
+                  padding: EdgeInsets.only(bottom: 16),
+                  child: Text('Add Event'),
+                ),
+                content: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: CupertinoTextField(
+                        placeholder: 'Event Title',
+                        onChanged: (value) {
+                          calendarButtonController.setTitle(value);
                         },
                       ),
-                      CupertinoDialogAction(
-                        child: const Text('Add Event'),
-                        onPressed: () async {
-                          SettingsModel settings =
-                              ref.watch(Providers.settingsControllerProvider);
-
-                          final Event newEvent = calendarButtonController
-                              .getEvent(settings.calendar?.id);
-                          _onSave(newEvent);
-
-                          Navigator.pop(context);
-                          // Event added successfully
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: CupertinoTextField(
+                        placeholder: 'Event Description',
+                        onChanged: (value) {
+                          calendarButtonController.setDescription(value);
                         },
                       ),
-                    ],
-                  );
-                });
-              },
-            );
-          }
-        }
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: CupertinoTextField(
+                        placeholder: 'Enter reminder minutes',
+                        keyboardType: TextInputType.number,
+                        onChanged: (value) {
+                          calendarButtonController
+                              .setReminderMinutes(int.parse(value));
+                        },
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                      ),
+                    ),
+                    const Text('Recurrence'),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: CupertinoSlidingSegmentedControl<int>(
+                        groupValue: calendarModel.selectedRecurrenceIndex,
+                        children: {
+                          for (int i = 0; i < recurrenceOptions.length; i++)
+                            i: Text(
+                                style: const TextStyle(
+                                  fontSize: 10,
+                                ),
+                                recurrenceOptions.values.elementAt(i)),
+                        },
+                        onValueChanged: (int? value) {
+                          calendarButtonController
+                              .setRecurrenceIndex(value ?? 0);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: DatePicker(
+                        description: "Start Time",
+                        dateTime: calendarModel.startDate!,
+                        onDateTimeChanged:
+                            calendarButtonController.setStartDate,
+                      ),
+                    ),
+                    DatePicker(
+                      description: "End Time",
+                      dateTime: calendarModel.endDate!,
+                      onDateTimeChanged: calendarButtonController.setEndDate,
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  CupertinoDialogAction(
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  CupertinoDialogAction(
+                    child: const Text('Add Event'),
+                    onPressed: () async {
+                      SettingsModel settings =
+                          ref.watch(Providers.settingsControllerProvider);
 
-        return CupertinoButton(
-          child: const Icon(CupertinoIcons.calendar_badge_plus),
-          onPressed: () {
-            showAddEventDialog(context);
+                      final Event newEvent = calendarButtonController
+                          .getEvent(settings.calendar?.id);
+                      _onSave(newEvent);
+
+                      Navigator.pop(context);
+                      // Event added successfully
+                    },
+                  ),
+                ],
+              );
+            });
           },
         );
+      }
+    }
+
+    return CupertinoButton(
+      child: const Icon(CupertinoIcons.calendar_badge_plus),
+      onPressed: () {
+        showAddEventDialog(context);
       },
     );
   }
